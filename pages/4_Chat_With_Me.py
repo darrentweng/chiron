c19d3c61478069e058dbd215770fcf9ec45bf3ec
darrentweng <darren.t.weng@gmail.com>
2022-10-08 17:13:33 -0400

    fix chatbot

--- pages/4_Chat_With_Me.py
+++ pages/4_Chat_With_Me.py
@@ -7,13 +7,17 @@ NoLinesUsed = 2
 co = cohere.Client(st.secrets["cohere_api"])
 
 emotions = [
-    Example("The order came 5 days early", "positive"), Example("The item exceeded my expectations", "positive"), 
-    Example("I ordered more for my friends", "positive"), Example("I would buy this again", "positive"), 
-    Example("I would recommend this to others", "positive"), Example("The package was damaged", "negative"), 
-    Example("The order is 5 days late", "negative"), Example("The order was incorrect", "negative"), 
-    Example("I want to return my item", "negative"), Example("The item\'s material feels low quality", "negative")]
+    Example("The order came 5 days early", "happy"), Example("The item exceeded my expectations", "happy"), 
+    Example("I ordered more for my friends", "happy"), Example("I would buy this again", "happy"), 
+    Example("I would recommend this to others", "happy"), Example("The package was damaged", "sad"), 
+    Example("The order is 5 days late", "sad"), Example("The order was incorrect", "sad"), 
+    Example("I want to return my item", "sad"), Example("The item\'s material feels low quality", "sad"),
+    Example("How do you use my item", "confused"), Example("What is this item for", "confused"),
+    Example("Why did I get this item", "confused"), Example("English makes no sense", "confused"),
+    Example("I don't understand physics", "confused")]
 
-st.title('Cohere.ai Demo')
+
+st.title('Talk to me')
 
 if 'chat_conv' not in st.session_state:
     st.session_state['chat_conv'] = ['Bot: Start typing below to chat with me.\n']
@@ -26,8 +30,9 @@ container_top.text(''.join(conv[-1*NoLinesDisplayed:]))
 container_bottom = st.empty()
 input = container_bottom.text_input("Your Message",value="",key="msg")
 
-#container_emotion = st.empty()
-#container_emotion.text("---")
+container_emotion = st.empty()
+container_emotion.text("---")
+
 
 if input != '':
     conv.append('You: '+input+'\n')
@@ -39,15 +44,16 @@ if input != '':
         frequency_penalty=0,   presence_penalty=0, return_likelihoods='NONE',
         stop_sequences=["User:", "\n"]
     ).generations[0].text.strip()
- 
+
     conv.append('Bot: '+response+'\n')
     container_top.text(''.join(conv[-1*NoLinesDisplayed:]))
-    input = container_bottom.text_input("Your Message",value="",key="placeholder")
+    # input = container_bottom.text_input("Your Message",value="",key="placeholder")
     st.session_state['chat_conv'] = conv[-1*NoLinesDisplayed:]
 
-    #emotion = co.classify( model='large', inputs=[input], examples=emotions) 
-    #e0 = emotion.classifications[0].confidence[0]
-    #e1 = emotion.classifications[0].confidence[1]
-    #container_emotion.text(e0.label+":"+str(e0.confidence)+" - "+e0.label+":"+str(e0.confidence))
+    emotion = co.classify( model='large', inputs=[input], examples=emotions) 
+    e0 = emotion.classifications[0].confidence[0]
+    e1 = emotion.classifications[0].confidence[1]
+    e2 = emotion.classifications[0].confidence[2]
+    container_emotion.text(e0.label+":"+str(e0.confidence) + " - "+e1.label+":"+str(e1.confidence)+" - " + e2.label+":"+str(e2.confidence))
    
 #st.write(response.generations[0].text)
